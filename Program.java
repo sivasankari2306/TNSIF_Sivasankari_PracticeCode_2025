ec82a435e3ad625b37493fd51395fffd2c164838
sivasankari2306 <146428476+sivasankari2306@users.noreply.github.com>
2025-07-07 14:14:07 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,11 @@
+package c2tc_day2;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Github");
+
+	}
+
+}
